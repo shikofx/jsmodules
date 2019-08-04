eacee1592c9d8373cb286567d2d3f628416c4fee
shikofx <shikofx@gmail.com>
2019-08-04 23:43:46 +0300

    Rejected study

--- lesson06-promises/static-reject.js
+++ lesson06-promises/static-reject.js
@@ -1,4 +1,4 @@
-
+//Обработка ошибок
 doAsyncWithReject = () => {
     return Promise.reject('It was error!!!');
 }
@@ -7,6 +7,21 @@ doAsyncWithReject()
 .then(
     null,
     (reason) => {
-        console.log(`Error ${reason}`);
+        console.log(`Rejected! ${reason}`);
     }
 );
+
+//Но невозможно отловить ошибку в колбэке:
+doAsync = () => {
+    console.log(`doAsync resolved!`);
+    return Promise.resolve(`OK. `);
+}
+
+doAsync().then(
+    (value)=> {
+        console.log(`${value} But after that error was generated!!!`)
+        throw new Error(`Error`);},
+    (reason) => {
+        console.log(`Rejected! ${reason}`);
+    }
+)
